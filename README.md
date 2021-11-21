994e48713b4036f1832654cfd6c3d0fe31ab0b58
chinabkevin <chinabalire.kevin.external@atos.net>
2021-11-21 19:22:00 +0300

    feat: add open api

--- README.md
+++ README.md
@@ -10,3 +10,13 @@
 
 - [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
 
+## Feature openapi documentation
+
+- [Micronaut OpenAPI Support documentation](https://micronaut-projects.github.io/micronaut-openapi/latest/guide/index.html)
+
+- [https://www.openapis.org](https://www.openapis.org)
+
+## Feature http-client documentation
+
+- [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
--- src/main/java/com/thinkdevs/Application.java
+++ src/main/java/com/thinkdevs/Application.java
@@ -1,7 +1,20 @@
 package com.thinkdevs;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Newsletter Subscriber",
+                version = "0.1",
+                description = "My API",
+                license = @License(name = "Apache 2.0", url = "https://foo.bar"),
+                contact = @Contact(url = "https://kevinjanvier.com", name = "Kevin", email = "kevin@kevinjanvier.com")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
