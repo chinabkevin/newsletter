e4e7d16b2e8dd6f0d170436485fa0b9bf7aaa6fd
chinabkevin <chinabalire.kevin.external@atos.net>
2021-11-26 00:51:39 +0300

    adds not found controller

---
+++ src/main/java/com/thinkdevs/controller/UnsubscribeController.java
@@ -0,0 +1,75 @@
+package com.thinkdevs.controller;
+
+import com.thinkdevs.api.v1.services.ConfirmationCodeVerifier;
+import com.thinkdevs.api.v1.services.UnsubscribeService;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Error;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.views.View;
+import io.micronaut.web.router.exceptions.UnsatisfiedQueryValueRouteException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Optional;
+
+@Controller("/unsubscribe")
+public class UnsubscribeController {
+
+    private final ConfirmationCodeVerifier confirmationCodeVerifier;
+    private final UnsubscribeService unsubscribeService;
+
+    public UnsubscribeController(ConfirmationCodeVerifier confirmationCodeVerifier, UnsubscribeService unsubscribeService) {
+        this.confirmationCodeVerifier = confirmationCodeVerifier;
+        this.unsubscribeService = unsubscribeService;
+    }
+
+    @Produces(MediaType.TEXT_HTML)
+    @View("unsubscribe")
+    @Get
+    HttpResponse<?> unsubscribe(@Nullable @QueryValue String token){
+        if (StringUtils.isEmpty(token)){
+            try {
+                return HttpResponse.seeOther(new URI("/404"));
+            }catch (URISyntaxException ex){
+                return HttpResponse.serverError();
+            }
+        }
+        Optional<String> emailOptional = confirmationCodeVerifier.verify(token);
+        if (!emailOptional.isPresent()){
+           return notFound();
+
+        }
+        unsubscribeService.unsubscribe(emailOptional.get());
+        return HttpResponse.ok(Collections.emptyMap());
+    }
+
+    private HttpResponse<?> notFound(){
+            try {
+                return HttpResponse.seeOther(new URI("/404"));
+            }catch (URISyntaxException ex){
+                return HttpResponse.serverError();
+            }
+
+    }
+
+    @Error
+    public HttpResponse<?> unsatisfiedQueryValueRouter(HttpRequest request,
+                                                       UnsatisfiedQueryValueRouteException exception){
+        try {
+            return HttpResponse.seeOther(new URI("/404"));
+        }catch (URISyntaxException ex){
+            return HttpResponse.serverError();
+        }
+    }
+
+
+
+}
---
+++ src/test/groovy/com/thinkdevs/controller/UnsubscribeControllerSpec.groovy
@@ -0,0 +1,78 @@
+package com.thinkdevs.controller
+
+import com.thinkdevs.api.v1.services.ConfirmationCodeGenerator
+import com.thinkdevs.api.v1.services.ConfirmationCodeVerifier
+import io.micronaut.core.annotation.Nullable
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
+import io.micronaut.http.client.BlockingHttpClient
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.uri.UriBuilder
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Shared
+import spock.lang.Specification
+
+@MicronautTest
+class UnsubscribeControllerSpec extends Specification {
+
+    @Inject
+    @Client("/")
+    HttpClient httpClient
+
+    @Shared
+    BlockingHttpClient _client
+
+    @Inject
+    ConfirmationCodeGenerator confirmationCodeGenerator;
+
+    BlockingHttpClient getClient(){
+        if (_client == null){
+            _client = httpClient.toBlocking()
+        }
+    }
+
+    void "Get /unsubscribe token query value parameters is required"(){
+        when: 'not token is supplied'
+        String html = client.retrieve(createRequest(), String)
+
+        then: 'app redirect to the 404'
+        noExceptionThrown()
+        html.contains('<h1>Not Found</h1>')
+    }
+
+    void "Get /unsubscribe if invalid token the user to 404"(){
+        when: 'not token is supplied'
+        String html = client.retrieve(createRequest("foo"), String)
+
+        then: 'app redirect to the 404'
+        noExceptionThrown()
+        html.contains('<h1>Not Found</h1>')
+    }
+
+    void "Get /unsubscribe token renders a Html Page telling the user unsuscription "(){
+        when: 'not token is supplied'
+        var token =confirmationCodeGenerator.generate("tcook@apple.com")
+
+        then:
+        token.isPresent()
+
+        String html = client.retrieve(createRequest(token.get()), String)
+
+        then: 'app show a happy path'
+        noExceptionThrown()
+        html.contains('<h1>You are no longer subscribed</h1>')
+    }
+
+    private static HttpRequest<?> createRequest(@Nullable String token){
+        UriBuilder builder =UriBuilder.of('/unsubscribe')
+        if (token){
+            builder.queryParam("token", token = null)
+        }
+        HttpRequest.GET(builder.build())
+                .accept(MediaType.TEXT_HTML)
+
+    }
+}
